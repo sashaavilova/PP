bc1f982ff3b848737e9eade0e2a842cffb4ecd16
Alexsandra Avilova <sasha.avilova.98@bk.ru>
2019-11-13 12:36:32 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpi.h"
-#define m 2
+#define m 8
 #define n 7
 #define count (n / 3 * m)
 
@@ -10,7 +10,7 @@
 void init(int *matrix)
 {    
      for (int i = 0; i < m*n; i++)
-         matrix[i] = 1;
+         matrix[i] = 1+i;
 }
 
 void print(int *source, int nn, int mm){
@@ -28,7 +28,9 @@ int main(int argc, char *argv[])
     int disp[count],
         len[count],
         *source = (int *)malloc(msize * sizeof(int)),
-        *result = (int *)malloc(msize * sizeof(int));
+        *result = (int *)malloc(msize * sizeof(int)),
+        *result_null = (int *)malloc(msize * sizeof(int)),
+        *rec;
     
    for (i = 0; i < count; i++)
     {
@@ -45,7 +47,9 @@ int main(int argc, char *argv[])
         }
         k += n % 3;
     }
-    
+
+   rec = (int *)malloc(msize * sizeof(int));
+
     int size, rank;
     
     MPI_Status status;    
@@ -54,8 +58,13 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Datatype newtype;
     
+    if(size!=2){
+        printf("Введите 2 процесса");
+        return 0;
+    }
     // count-количество блоков; *len- количество элементов в каждом блоке; *disp-расстояние между блоками, передоваемые данные
-    MPI_Type_indexed(count, len, disp, MPI_INTEGER, &newtype);MPI_Type_commit(&newtype);
+    MPI_Type_indexed(count, len, disp, MPI_INTEGER, &newtype);
+    MPI_Type_commit(&newtype);
     
     if (rank == 0)
     {
@@ -65,6 +74,12 @@ int main(int argc, char *argv[])
         printf("\nSource matrix: \n");
         print(source, n, m);
         MPI_Send(source, 1, newtype, 1, 1, MPI_COMM_WORLD);
+///////////////////////////
+        MPI_Recv(rec, 1, newtype, 1, 1, MPI_COMM_WORLD, &status);
+        printf("\nRunning %d of rang", rank);
+        printf("\nNULL matrix: \n");
+        print(rec, n, m);
+   
     }
 
     if (rank != 0)
@@ -72,7 +87,14 @@ int main(int argc, char *argv[])
         MPI_Recv(result, 1, newtype, 0, 1, MPI_COMM_WORLD, &status);
         printf("\nResult matrix: \n");
         print(result, n, m);
+
+         MPI_Send(result, 1, newtype, 0, 1, MPI_COMM_WORLD);
     }
+
+       
+
+
+    free(result_null);
     free(source);
     free(result);
     MPI_Type_free(&newtype);
